763110bacecf5ed37d3c25d595995503711eea50
prathamesh <prathamesh69bhavsar@gmail.com>
2024-11-18 19:40:44 +0530

    reverse

--- Test.java
+++ Test.java
@@ -1,20 +1,13 @@
 public class Test{
-    public static void middle(int arr[]){
-        int mid;
-       
-            if(arr.length % 2 == 0){
-                mid = arr.length / 2;
-                System.out.println(arr[mid - 1]+" "+arr[mid]);
-            }
-            else{
-                mid = arr.length / 2;
-                System.out.println(arr[mid]);
-            }
-    
+    public static void ReverseArray(int arr[]){
+        for(int i = arr.length - 1; i >= 0; i--)
+        {
+            System.out.print(arr[i]+" ");
+        }
     }
 
     public static void main(String ar[]){
         int arr[] = { 2, 14, 5, 5};
-        middle(arr);    
+        ReverseArray(arr);
     }
 }
